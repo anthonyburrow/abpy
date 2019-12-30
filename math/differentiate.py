496b32099d95f849f1f0c792633a3b9c41938fc9
Anthony Burrow <anthony.r.burrow@gmail.com>
2019-12-30 00:46:55 -0600

    More cleaning

--- math/differentiate.py
+++ math/differentiate.py
@@ -1,50 +1,20 @@
-def Derivative(func, x, eps=0.00001, dx0=1, direction='symmetric'):
-    dx = dx0
-    fp0 = 0
-    count = 0
-    while True:
-        if direction == 'right':
-            x0, x1 = (x, x + dx)
-            fp1 = (func(x1) - func(x0)) / dx
-        elif direction == 'left':
-            x0, x1 = (x - dx, x)
-            fp1 = (func(x1) - func(x0)) / dx
-        elif direction == 'symmetric':
-            x0, x1 = (x - dx, x + dx)
-            fp1 = (func(x1) - func(x0)) / (2 * dx)
+def derivative(X, func, dx=0.000001, *args, **kwargs):
+    """Calculate the derivative at point(s).
 
-        if count != 0:
-            if abs(fp1 - fp0) < eps:
-                return fp1
+    Args:
+        X (float, numpy.ndarray): Input points.
+        func: Function of which to take derivative. Must take input as first
+              parameter.
+        dx (float): Step size.
 
-        fp0 = fp1
-        dx /= 2
-        count += 1
+    Returns:
+        fprime (float, numpy.ndarray): Derivative of function at inputs.
 
+    """
+    x0 = X - dx
+    x1 = X + dx
+    f0 = func(x0, *args, **kwargs)
+    f1 = func(x1, *args, **kwargs)
+    fprime = 0.5 * (f1 - f0) / dx
 
-def nDerivative(func, x, order, eps=0.00001, dx0=1):
-    if order < 0:
-        print('Order of derivative must be a nonnegative integer.')
-        return
-    if order == 0:
-        return func(x)
-
-    from .combinatorics import Combination
-
-    dx = dx0
-    fp0 = 0
-    count = 0
-    while True:
-        s = 0
-        for k in range(order + 1):
-            s += (-1)**(k + 1) * Combination(order, k) * func(x + k * dx)
-
-        fp1 = s / (dx**order)
-
-        if count != 0:
-            if abs(fp1 - fp0) < eps:
-                return fp1
-
-        fp0 = fp1
-        dx /= 2
-        count += 1
+    return fprime
--- math/gauss.py
+++ math/gauss.py
@@ -2,52 +2,30 @@ import numpy as np
 from scipy.optimize import curve_fit
 
 
-def gauss(x, mu, sigma, A):
-    return A * np.exp(-(x - mu)**2 / 2 / sigma**2)
+def gauss(x, mu, sig, A):
+    """1-D Gaussian function"""
+    arg = (x - mu) / sig
+    return A * np.exp(-0.5 * arg**2)
 
 
-def bimodal(x, mu1, sigma1, A1, mu2, sigma2, A2):
-    return gauss(x, mu1, sigma1, A1) + gauss(x, mu2, sigma2, A2)
+def gauss_fit(X, mu0=0, sig0=1, hist_bins=40, fit_bounds=(-np.inf, np.inf)):
+    """Retrieve a 1-D Gaussian fit.
 
+    Args:
+        X (numpy.ndarray): Data to be fit.
+        mu0 (float): Initial mean to start approximation.
+        sig0 (float): Initial deviation to start approximation.
+        hist_bins (int): Number of bins to use for histogram.
 
-def hist_fit(x_arr, p0=[0, 0.5], hist_bins=10, hist_range=None,
-             fit_bounds=(-np.inf, np.inf)):
-    # p0 has the form mu, sigma
-    h, x = np.histogram(x_arr, bins=hist_bins, range=hist_range, normed=False)
+    Returns:
+        params (array): Optimal values of parameters for fit (mu, sigma, amplitude).
+        cov (array): 2D covariance matrix for parameters.
+
+    """
+    h, x = np.histogram(X, bins=hist_bins, normed=False)
     x = (x[1:] + x[:-1]) / 2  # get centers of bins
 
-    p0 = p0 + [h.max()]
-
+    p0 = [mu0, sig0, h.max()]
     params, cov = curve_fit(gauss, x, h, p0=p0, bounds=fit_bounds)
-
-    return params
-
-
-def hist_fit_bimodal(x_arr, p0=[0, 0.5, 10, 0.5], hist_bins=50, hist_range=None,
-                     fit_bounds=(-np.inf, np.inf)):
-    # p0 has the form mu1, sigma1, m2, sigma2
-    h, x = np.histogram(x_arr, bins=hist_bins, range=hist_range, normed=False)
-    x = (x[1:] + x[:-1]) / 2  # get centers of bins
-
-    p0 = p0[0:2] + [h.max() / 2] + p0[2:4] + [h.max()]
-
-    params, cov = curve_fit(bimodal, x, h, p0=p0, bounds=fit_bounds)
-
-    return params
-
-
-def hist_fit_2d(x_arr, y_arr, p0=[0, 0, 0.5, 0.5], hist_bins=50, hist_range=None,
-                fit_bounds=(-np.inf, np.inf)):
-    # p0 has the form mux, muy, sigma_x, sigma_y
-    h, x, y = np.histogram2d(x_arr, y_arr, bins=hist_bins, range=hist_range,
-                             normed=False)
-    x = np.array((x[1:] + x[:-1]) / 2)
-    y = np.array((y[1:] + y[:-1]) / 2)
-    x, y = np.meshgrid(x, y)
-    h = h.ravel()
-
-    p0 = p0 + [h.max()]
-
-    params, cov = curve_fit(Gauss2D, (x, y), h, p0=p0)
 
     return params, cov
