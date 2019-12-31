28311f5777580ee4ca4c565d808685a1cbd214b5
Anthony Burrow <anthony.r.burrow@gmail.com>
2019-12-31 12:38:40 -0600

    finish save state

--- util/process.py
+++ util/process.py
@@ -3,56 +3,125 @@ import pickle
 
 class StateManager:
 
-    def __init__(self, manager_name, path='./'):
-        """Create state manager."""
+    def __init__(self, manager_name: str, path: str='./'):
+        """Create state manager.
+
+        Args:
+            manager_name (str): Name of manager and state file.
+            path (str): Path to place file.
+
+        """
         self.filename = f'{path}{manager_name}.p'
 
         self._state = {}
-        self._setup()
-
-    def _setup(self):
-
         try:
             self._state = pickle.load(open(self.filename, 'rb'))
         except (FileNotFoundError, EOFError):
-            # Create 'blank' file with only {}
-            self._save_state()
-
-    def _save_state(self):
-        """Save the current state."""
-        pickle.dump(self._state, open(self.filename, 'wb'))
+            # Create 'blank' file with only { }
+            self.save()
 
     def new(self, name: str):
+        """Add new process to state to be completed.
+
+        Args:
+            name (str): Name of process.
+
+        """
         self._state[name] = {'data': None, 'complete': False}
 
     def remove(self, name: str):
+        """Remove process from state.
+
+        Args:
+            name (str): Name of process.
+
+        """
         self._state.pop(name, None)
 
-    def save(self, name: str, data):
-        self._state[name]['data'] = data
-        self._state[name]['complete'] = True
-        self._save_state()
+    def complete(self, name: str, data):
+        """Add in completed process.
+
+        Args:
+            name (str): Name of process.
+            data: Data (of any type) output from process.
+
+        """
+        self._state[name] = {'data': data, 'complete': True}
 
     def get(self, name: str):
-        return self._state[name]['data']
+        """Get data associated with process.
+
+        Args:
+            name (str): Name of process.
+
+        Returns:
+            data: Data output from process.
+
+        """
+        try:
+            data = self._state[name]['data']
+            return data
+        except KeyError:
+            return
+
+    def is_process(self, name: str):
+        """Check if process is in state.
+
+        Args:
+            name (str): Name of process.
+
+        Returns:
+            is_proc (bool): True if is process.
+
+        """
+        is_proc = name in self._state
+        return is_proc
 
     def is_complete(self, name: str):
-        return self._state[name]['complete']
+        """Check if process is complete.
+
+        Args:
+            name (str): Name of process.
+
+        Returns:
+            is_comp (bool): True if complete.
+
+        """
+        is_comp = self._state[name]['complete']
+        return is_comp
 
     def get_complete(self):
+        """Yield processes that are complete."""
         for name in self._state:
             if self._state[name]['complete']:
                 yield name
 
     def get_incomplete(self):
+        """Yield processes that are incomplete."""
         for name in self._state:
             if not self._state[name]['complete']:
                 yield name
 
+    def save(self):
+        """Save the current state."""
+        pickle.dump(self._state, open(self.filename, 'wb'))
+
     def reset(self, name: str):
+        """Set process as incomplete.
+
+        Args:
+            name (str): Name of process.
+
+        """
         self._state[name]['complete'] = False
 
     def reset_all(self):
+        """Set every process in state as incomplete."""
         for name in self._state:
             self.reset(name)
-        self._save_state()
+
+    @property
+    def count(self):
+        """Get number of processes in state."""
+        num = len(self._state)
+        return num
