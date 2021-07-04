5e084ca2cecd91cfdc4001f3acb15cce1bcab570
Anthony Burrow <anthony.r.burrow@gmail.com>
2021-07-04 09:54:13 -0500

    added constants

---
+++ abpy/astro/const.py
@@ -0,0 +1,10 @@
+# CGS
+
+# Physics Constants
+c = 2.99792458e10   # cm s^{-1}
+h = 6.6261e-27      # cm^2 g s^{-1}
+k_B = 1.3807e-16    # cm^2 g s^{-2} K^{-1}
+G = 6.6743e-8       # cm^3 g^{-1} s^{-2}
+
+# Astronomical Constants
+m_sol = 1.989e33    # g
